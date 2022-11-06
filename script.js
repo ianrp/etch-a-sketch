f636a4a81b5d0fc7f7edd746f012fffd389a801d
ianrp <irpennin@gmail.com>
2022-11-06 05:05:04 -0500

    Add event listeners to change tile color when hovered over

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridSize = 16;
+const trailColor = "blue";
 
 generateGrid();
 
@@ -7,7 +8,13 @@ function generateGrid() {
     for (let r = 0; r < gridSize; r++) {
         for (let c = 0; c < gridSize; c++) {
             const tile = document.createElement("div");
+            tile.addEventListener("mouseenter", changeTileColor);
             grid.appendChild(tile);
         }
     }
+}
+
+function changeTileColor() {
+    this.style.backgroundColor = trailColor;
+    this.removeEventListener("mouseenter", changeTileColor);
 }
